b8acf71682da2a1765c51163f077969e6b30bb9e
박서준 <bakseo3060@gmail.com>
2021-01-23 00:52:09 +0900

    Edwidth Kotlin 기본 강의 소스 코드 정리

---
+++ src/chap02/section3/NumberTest.kt
@@ -0,0 +1,17 @@
+package chap02.section3
+
+fun main() {
+    //스마트 캐스트
+    var test: Number = 12.2
+    println("$test")
+
+
+    test = 12
+    println("$test")
+
+    test = 120L
+    println("$test")
+
+    test += 12.0f
+    println("$test")
+}
